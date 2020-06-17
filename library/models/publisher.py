416426192a79b40e497fa15b8b83bd2863f7a46d
mihaivladu <mihai@academicmerit.com>
2020-06-17 09:37:50 +0300

    Week 12 - Recap.

--- library/models/publisher.py
+++ library/models/publisher.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Publisher(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='publishers')
     name = models.CharField(unique=True, max_length=255)
 
     def __str__(self):
